707f0e15830f45a36555c90e293fe265ed380b38
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2025-02-15 16:24:33 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,9 @@
 // Importando o Firebase corretamente pelo CDN
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
 
 
